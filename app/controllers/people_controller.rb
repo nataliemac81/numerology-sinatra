e970d6ee21b3d8494eba0246b9aa7e57314ee341
Natalie McCroy <nataliemccroy@gmail.com>
2018-04-02 15:31:52 -0500

    add partials for new/edit person forms

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,18 +1,11 @@
-get '/' do
+get '/people' do
 	@people = Person.all
 	erb :"/people/index"
 end
 
-get '/new' do
+get '/people/new' do
+	@person = Person.new
 	erb :"/people/new"
-end
-
-get '/:id' do
-	@person = Person.find(params[:id])
-	birthdate_string = @person.birthdate.strftime("%m%d%Y")
-	bpn = Person.determine_birth_path_number(birthdate_string)
-	@message = Person.determine_message(bpn)
-	erb :"/people/show"
 end
 
 post '/people' do
@@ -24,4 +17,32 @@ post '/people' do
 	person = Person.create(first_name: params[:first_name],
 	last_name: params[:last_name], birthdate: params[:birthdate])
 	redirect "/people/#{person.id}"
+end
+
+get '/people/:id/edit' do
+	@person = Person.find(params[:id])
+	erb :"/people/edit"
+end
+
+get '/people/:id' do
+	@person = Person.find(params[:id])
+	birthdate_string = @person.birthdate.strftime("%m%d%Y")
+	bpn = Person.determine_birth_path_number(birthdate_string)
+	@message = Person.determine_message(bpn)
+	erb :"/people/show"
+end
+
+put '/people/:id' do
+	person = Person.find(params[:id])
+	person.first_name = params[:first_name]
+	person.last_name = params[:last_name]
+	person.birthdate = params[:birthdate]
+	person.save
+	redirect "/people/#{person.id}"
+end
+
+delete '/people/:id' do
+	person = Person.find(params[:id])
+	person.destroy
+	redirect "/people"
 end
